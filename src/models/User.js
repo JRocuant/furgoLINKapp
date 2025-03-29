6c4ea168df40f3bb9282a379d0ffe6ce3f07e026
Jrocuant <jrocuant@gmail.com>
2025-03-28 21:45:33 -0300

    id agregado como string en modelo/schema user

--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,31 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
-    name: { type: String, required: true},
-    email: { type: String, required: true, unique: true},
-    password: { type: String, required: true},
-
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    id: { type: String }  // Agregar un campo id como string
 }, {
     timestamps: true
 });
 
+// Hook pre-save para asignar el _id como string al campo id
+UserSchema.pre('save', function(next) {
+    if (this.isNew) {  // Si el documento es nuevo
+        this.id = this._id.toString();  // Convertir el _id a string y guardarlo en id
+    }
+    next();
+});
+
+// Métodos para encriptar y comparar la contraseña
 UserSchema.methods.encriptarPassword = async password => {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
 };
 
 UserSchema.methods.compararPassword = async function(password) {
-    return await bcrypt.compare(password, this.password)
-}
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = model('User', UserSchema);
-
-
-
-//
